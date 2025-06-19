388a2f7175231b996ad4cfbbde30a65513561799
AyanaAkutsu <s1290002.u.aizu@gmail.com>
2025-06-19 20:36:30 +0900

    見た目の調整

--- pages/top.py
+++ pages/top.py
@@ -7,23 +7,38 @@ st.set_page_config(
 st.markdown(
     """
     <style>
+    @import url(
+        'https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap'
+    );
     .stApp{
         background-color: #E5F2F0;
     }
 
-    div.stTitle > title {
+    div.stHeading > div > div > h1 {
+        display: flex;
+        justify-content: center;
+        color: #194352;
         font-size: 40px;
-        font-weight: semi bold;
+        font-family: "Inter", sans-serif;
+        font-weight: 600;
     }
 
     div.stButton > button {
         width: 345px;
         height: 95px;
         color: #349AA6;
+        border-radius: 20px;
+        box-shadow: 0px 4px 4px rgba(0,0,0,0.25);
+    }
+    div.stButton > button > div{
+    }
+    div.stButton > button > div > p{
         font-size: 32px;
-        font-weight: semi bold;
-        border: none;
-        box-shadow: 0px, 4px, 4px, rgba(0,0,0,0.25);
+        font-family: "Inter", sans-serif;
+        font-weight: 600;
+    }
+    div.stButton > button:hover {
+        background-color: #00000;
     }
     </style>
     """,
