7bd04824b8bbbbc4d173787ed0efc98081922f3a
AyanaAkutsu <s1290002.u.aizu@gmail.com>
2025-06-27 20:32:50 +0900

    文字等の見た目調整

--- pages/top.py
+++ pages/top.py
@@ -7,38 +7,93 @@ st.set_page_config(
 st.markdown(
     """
     <style>
-    @import url(
-        'https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap'
-    );
     .stApp{
         background-color: #E5F2F0;
     }
 
+    div[data-testid="stToolbar"] {
+        visibility: hidden;
+        height: 0%;
+        position: fixed;
+    }
+    div[data-testid="stDecoration"] {
+        visibility: hidden;
+        height: 0%;
+        position: fixed;
+    }
+    #MainMenu {
+        visibility: hidden;
+        height: 0%;
+    }
+    header {
+        visibility: hidden;
+        height: 0%;
+    }
+    footer {
+        visibility: hidden;
+        height: 0%;
+    }
+    .appview-container .main .block-container{
+        padding-top: 1rem;
+        padding-right: 3rem;
+        padding-left: 3rem;
+        padding-bottom: 1rem;
+    }
+    .reportview-container {
+        padding-top: 0rem;
+        padding-right: 3rem;
+        padding-left: 3rem;
+        padding-bottom: 0rem;
+    }
+    header[data-testid="stHeader"] {
+        z-index: -1;
+    }
+    div[data-testid="stToolbar"] {
+    z-index: 100;
+    }
+    div[data-testid="stDecoration"] {
+    z-index: 100;
+    }
+    @import url(
+        'https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap'
+    );
+
     div.stHeading > div > div > h1 {
         display: flex;
         justify-content: center;
-        color: #194352;
         font-size: 40px;
         font-family: "Inter", sans-serif;
         font-weight: 600;
+        background-color: #194352;
+        color: transparent;
+        text-shadow: 2px 2px 3px rgba(255,255,255,0.3);
+        -webkit-background-clip: text;
+           -moz-background-clip: text;
+                background-clip: text;
     }
 
     div.stButton > button {
+        display: flex;
+        justify-content: center;
         width: 345px;
         height: 95px;
         color: #349AA6;
         border-radius: 20px;
         box-shadow: 0px 4px 4px rgba(0,0,0,0.25);
     }
-    div.stButton > button > div{
-    }
     div.stButton > button > div > p{
         font-size: 32px;
         font-family: "Inter", sans-serif;
         font-weight: 600;
+        background-color: #349AA6;
+        color: transparent;
+        text-shadow: 2px 2px 3px rgba(255,255,255,0.3);
+        -webkit-background-clip: text;
+           -moz-background-clip: text;
+                background-clip: text;
     }
-    div.stButton > button:hover {
-        background-color: #00000;
+    [data-testid="stButton"]::after {
+        background-color: #67b3bc;
     }
     </style>
     """,
